8d647286d77fc8a99731d057880f5dac021956fc
pilpod <67429595+pilpod@users.noreply.github.com>
2020-10-21 18:31:57 +0200

    Inicializacion de la Kata RPG combat kata

---
+++ Readme.md
@@ -0,0 +1,14 @@
+# RPG Combat Kata
+
+## Requiered
+
+- PHP 7.4
+- Composer installed
+
+## Install
+
+- composer install
+
+## Run Tests
+
+- vendor/bin/phpunit
---
+++ tests/ExampleTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Tests;
+
+use PHPUnit\Framework\TestCase;
+use App\RpgKombat;
+
+class ExampleTest extends TestCase {
+
+	public function test_example(
+	) {
+		
+		$this->assertEquals(4, 4);
+	}
+
+}
+
+
